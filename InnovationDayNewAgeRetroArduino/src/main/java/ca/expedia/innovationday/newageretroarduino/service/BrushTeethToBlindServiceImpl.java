ac3a10679bfd9faa56bbf355c4fe8742e3acdbc6
rkovessy <rogerkovessy@gmail.com>
2013-12-02 15:54:48 -0500

    Updating to add who failed the build

--- InnovationDayNewAgeRetroArduino/src/main/java/ca/expedia/innovationday/newageretroarduino/service/BrushTeethToBlindServiceImpl.java
+++ InnovationDayNewAgeRetroArduino/src/main/java/ca/expedia/innovationday/newageretroarduino/service/BrushTeethToBlindServiceImpl.java
@@ -10,6 +10,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,15 +23,36 @@ public class BrushTeethToBlindServiceImpl implements BrushTeethToBlindService {
 	JenkinsProjectInfo projectInfo;
 	
 	public void brushTeeth() {
-		List<String> response = getJenkinsResponse();
+		List<String> response = getJenkinsResponse("http://chellssbuild002:8080/jenkins/view/Lodging%20Services/job/lodging.services_content_score/api/json");
 		projectInfo = buildProjectInfo(response.get(0));
 	
-		if (projectInfo.getCurrentBuildNumber() == projectInfo.getFailBuildNumber()) {
+		if (projectInfo.getCurrentBuildNumber() != projectInfo.getSuccessBuildNumber()) {
 			System.out.println("loud noises!");
+			System.out.println("Build breaker: "+getNameOfHeathen(projectInfo.getCurrentBuildNumber()));
 			nastySound.on(1000);
 		} else {
 			System.out.println("All good");
 		}
+	}
+	
+	private String getNameOfHeathen(long projectID) {
+		String url = "http://chellssbuild002:8080/jenkins/view/Lodging%20Services/job/lodging.services_content_score/"+Long.toString(projectID)+"/api/json";
+		System.out.println(url);
+		List<String> response = getJenkinsResponse(url);
+		JSONParser parser = new JSONParser();
+		String heathen = "";
+		try {
+			Object obj = parser.parse(response.get(0));
+			JSONObject jsonObject = (JSONObject) obj;
+			
+			JSONArray culprits = (JSONArray) jsonObject.get("culprits");
+			JSONObject culprit = (JSONObject) culprits.get(0);
+			heathen = (String) culprit.get("fullName");
+		} catch (org.json.simple.parser.ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return heathen;
 	}
 	
 	private JenkinsProjectInfo buildProjectInfo(String input) {
@@ -55,12 +77,12 @@ public class BrushTeethToBlindServiceImpl implements BrushTeethToBlindService {
 		return projectInfo;
 	}
 	
-	private List<String> getJenkinsResponse() {
+	private List<String> getJenkinsResponse(String url) {
 		List<String> output = new ArrayList<String>();
 		
 		try {
 			DefaultHttpClient httpClient = new DefaultHttpClient();
-			HttpGet getRequest = new HttpGet("http://chellssbuild002:8080/jenkins/job/lodging.services_content_score_mediakpi/api/json");
+			HttpGet getRequest = new HttpGet(url);
 			
 			HttpResponse response = httpClient.execute(getRequest);
 			
